4463665dcd9ed17a4dce221c3992b1d777748310
915-Micu-AlexiaClaudia <alexiamicu@gmail.com>
2025-01-04 22:31:24 +0100

    Follow the Leader + starting artefact

--- Hair_Simulation/Assets/Components/HairStrand.cs
+++ Hair_Simulation/Assets/Components/HairStrand.cs
@@ -15,17 +15,7 @@ public class HairStrand : MonoBehaviour
         this.Vertices = new List<StrandVertex>();
         this.Springs = new List<StrandSpring>();
 
-        StrandVertex vertex0 = new StrandVertex(new Vector3(0    , 6f  , 0), Constants.StraightHairMass, true); this.Vertices.Add(vertex0);
-        StrandVertex vertex1 = new StrandVertex(new Vector3(0.4f , 5.6f, 0), Constants.StraightHairMass); this.Vertices.Add(vertex1);
-        StrandVertex vertex2 = new StrandVertex(new Vector3(0    , 5.2f, 0), Constants.StraightHairMass); this.Vertices.Add(vertex2);
-        StrandVertex vertex3 = new StrandVertex(new Vector3(-0.4f, 4.8f, 0), Constants.StraightHairMass); this.Vertices.Add(vertex3);
-        StrandVertex vertex4 = new StrandVertex(new Vector3(0.0f , 4.4f, 0), Constants.StraightHairMass); this.Vertices.Add(vertex4);
-        StrandVertex vertex5 = new StrandVertex(new Vector3(0.4f , 4.0f, 0), Constants.StraightHairMass); this.Vertices.Add(vertex5);
-        StrandVertex vertex6 = new StrandVertex(new Vector3(0.0f , 3.6f, 0), Constants.StraightHairMass); this.Vertices.Add(vertex6);
-        StrandVertex vertex7 = new StrandVertex(new Vector3(-0.4f, 3.2f, 0), Constants.StraightHairMass); this.Vertices.Add(vertex7);
-        StrandVertex vertex8 = new StrandVertex(new Vector3(0.0f , 2.8f, 0), Constants.StraightHairMass); this.Vertices.Add(vertex8);
-        StrandVertex vertex9 = new StrandVertex(new Vector3(0.4f , 2.4f, 0), Constants.StraightHairMass); this.Vertices.Add(vertex9);
-        StrandVertex vertex10 = new StrandVertex(new Vector3(0.0f , 2.0f, 0), Constants.StraightHairMass); this.Vertices.Add(vertex10);
+        this.CurlyStrand();
 
         AddSpring(0, 1);
         AddSpring(1, 2);
@@ -39,15 +29,46 @@ public class HairStrand : MonoBehaviour
         AddSpring(9, 10);
     }
 
+    void CurlyStrand()
+    {
+        StrandVertex vertex0 = new StrandVertex(new Vector3(0, 6f, 0), Constants.StraightHairMass, true); this.Vertices.Add(vertex0);
+        StrandVertex vertex1 = new StrandVertex(new Vector3(0.4f, 5.6f, 0), Constants.StraightHairMass); this.Vertices.Add(vertex1);
+        StrandVertex vertex2 = new StrandVertex(new Vector3(0, 5.2f, 0), Constants.StraightHairMass); this.Vertices.Add(vertex2);
+        StrandVertex vertex3 = new StrandVertex(new Vector3(-0.4f, 4.8f, 0), Constants.StraightHairMass); this.Vertices.Add(vertex3);
+        StrandVertex vertex4 = new StrandVertex(new Vector3(0.0f, 4.4f, 0), Constants.StraightHairMass); this.Vertices.Add(vertex4);
+        StrandVertex vertex5 = new StrandVertex(new Vector3(0.4f, 4.0f, 0), Constants.StraightHairMass); this.Vertices.Add(vertex5);
+        StrandVertex vertex6 = new StrandVertex(new Vector3(0.0f, 3.6f, 0), Constants.StraightHairMass); this.Vertices.Add(vertex6);
+        StrandVertex vertex7 = new StrandVertex(new Vector3(-0.4f, 3.2f, 0), Constants.StraightHairMass); this.Vertices.Add(vertex7);
+        StrandVertex vertex8 = new StrandVertex(new Vector3(0.0f, 2.8f, 0), Constants.StraightHairMass); this.Vertices.Add(vertex8);
+        StrandVertex vertex9 = new StrandVertex(new Vector3(0.4f, 2.4f, 0), Constants.StraightHairMass); this.Vertices.Add(vertex9);
+        StrandVertex vertex10 = new StrandVertex(new Vector3(0.0f, 2.0f, 0), Constants.StraightHairMass); this.Vertices.Add(vertex10);
+    }
+
+    void StraightStrand()
+    {
+        StrandVertex vertex0 = new StrandVertex(new Vector3(0, 6f, 0), Constants.StraightHairMass, true); this.Vertices.Add(vertex0);
+        StrandVertex vertex1 = new StrandVertex(new Vector3(0, 5.4f, 0), Constants.StraightHairMass); this.Vertices.Add(vertex1);
+        StrandVertex vertex2 = new StrandVertex(new Vector3(0, 4.8f, 0), Constants.StraightHairMass); this.Vertices.Add(vertex2);
+        StrandVertex vertex3 = new StrandVertex(new Vector3(0, 4.2f, 0), Constants.StraightHairMass); this.Vertices.Add(vertex3);
+        StrandVertex vertex4 = new StrandVertex(new Vector3(0, 3.6f, 0), Constants.StraightHairMass); this.Vertices.Add(vertex4);
+        StrandVertex vertex5 = new StrandVertex(new Vector3(0, 3.0f, 0), Constants.StraightHairMass); this.Vertices.Add(vertex5);
+        StrandVertex vertex6 = new StrandVertex(new Vector3(0, 2.4f, 0), Constants.StraightHairMass); this.Vertices.Add(vertex6);
+        StrandVertex vertex7 = new StrandVertex(new Vector3(0, 1.8f, 0), Constants.StraightHairMass); this.Vertices.Add(vertex7);
+        StrandVertex vertex8 = new StrandVertex(new Vector3(0, 1.2f, 0), Constants.StraightHairMass); this.Vertices.Add(vertex8);
+        StrandVertex vertex9 = new StrandVertex(new Vector3(0, 0.6f, 0), Constants.StraightHairMass); this.Vertices.Add(vertex9);
+        StrandVertex vertex10 = new StrandVertex(new Vector3(0, 0f, 0), Constants.StraightHairMass); this.Vertices.Add(vertex10);
+    }
+
     void AddSpring(int from, int to)
     {
         float restLength = (Vertices[to].Position - Vertices[from].Position).magnitude;
-        Springs.Add(new StrandSpring(from, to, restLength, Constants.StraightHairStiffness, Constants.StraightHairDamping));
+        Springs.Add(new StrandSpring(from, to, restLength));
     }
 
     void Update()
     {
         SimulateStrand();
+        ApplyFollowTheLeader();
         UpdateVertices();
     }
 
@@ -57,7 +78,7 @@ public class HairStrand : MonoBehaviour
 
         foreach (var vertex in this.Vertices)
         {
-            if(vertex.isRoot)
+            if (vertex.isRoot)
             {
                 Vector3 emitterVelocity = emitter.GetComponent<Rigidbody>().linearVelocity;
                 vertex.Velocity = emitterVelocity;
@@ -83,23 +104,34 @@ public class HairStrand : MonoBehaviour
 
             Vector3 direction = vertexTo.Position - vertexFrom.Position;
             float currentLength = direction.magnitude;
-
             direction = direction.normalized;
 
-            float springForceMagnitude = spring.Stiffness * (currentLength - spring.RestLength); // displacement from the rest position
+            float springForceMagnitude = Constants.StraightHairStiffness * (currentLength - spring.Length);
             Vector3 springForce = springForceMagnitude * direction;
 
             Vector3 relativeVelocity = vertexTo.Velocity - vertexFrom.Velocity;
-            Vector3 dampingForce = spring.Damping * relativeVelocity;
+            Vector3 dampingForce = Constants.StraightHairDamping * relativeVelocity;
 
             Vector3 vertexFrom_Force = springForce + dampingForce;
             Vector3 vertexTo_Force = -vertexFrom_Force;
 
-            if(!vertexFrom.isRoot)
+            if (!vertexFrom.isRoot)
             {
                 vertexFrom.Velocity += (vertexFrom_Force / vertexFrom.Mass) * deltaTime;
             }
             vertexTo.Velocity += (vertexTo_Force / vertexTo.Mass) * deltaTime;
+        }
+    }
+
+    void ApplyFollowTheLeader()
+    {
+        for (int i = 1; i < Vertices.Count; i++)
+        {
+            Vector3 previousPosition = Vertices[i - 1].Position;
+            Vector3 direction = (Vertices[i].Position - previousPosition).normalized;
+            float restLength = Springs[i - 1].Length;
+
+            Vertices[i].Position = previousPosition + direction * restLength;
         }
     }
 }
--- Hair_Simulation/Assets/Utils/Constants.cs
+++ Hair_Simulation/Assets/Utils/Constants.cs
@@ -4,8 +4,11 @@ public class Constants
 {
     public static readonly Vector3 Gravity = new Vector3(0, -9.81f, 0);
 
-    public static readonly float StraightHairStiffness = 1f;
+    public static readonly float StraightHairStiffness = 0.99f;
     public static readonly float StraightHairDamping = 0.3f;
     public static readonly float StraightHairMass = 0.01f;
     public static readonly float StraightHairThickness = 0.05f;
+
+    public static readonly float BendingStiffness = 1f;
+    public static readonly float BendingDamping = 1f;
 }
